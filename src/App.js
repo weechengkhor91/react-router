dd97b39a2c319600fcfdf3e54768e39501089221
weechengkhor <chengkhor798@gmail.com>
2019-05-21 17:41:29 +0800

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import About from "./routes/About/about";
 import Contact from "./routes/Contact/contact";
 import Home from "./routes/Home/home";
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,15 +1,27 @@
 import React, {Component, Fragment} from 'react'
 import {Link} from 'react-router-dom'
 
+//styling
+import './navbar.less'
+
 class Navbar extends Component{
+
     render(){
+
+        //console.log(window.location)
+        const {location} = window
+        const {pathname} = location
         return(
             <Fragment>
+                <div className="navbar-outer">
+                    <div className="navbar-wrapper">
                 <ul className="navbar-list">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <li className={pathname === '/' ? 'active' : ''}><a href="/">Home</a></li>
+                    <li className={pathname === '/about' ? 'active' : ''}><a href="/about" >About</a></li>
+                    <li className={pathname === '/contact' ? 'active' : ''}><a href="/contact">Contact</a></li>
                 </ul>
+                    </div>
+                </div>
             </Fragment>
         )
     }
